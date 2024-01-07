3c0dfe8f85c95f202ae9656f99d6b80f13ee6f1a
sv-aloksingh <aloksingh8979@gmail.com>
2024-01-07 11:48:09 -0800

    print 1 to 10

---
+++ src/DSALectureByFrazPulkit/LectureProgramsPractice/Print1toN.java
@@ -0,0 +1,16 @@
+package DSALectureByFrazPulkit.LectureProgramsPractice;
+import java.util.Scanner;
+
+public class Print1toN {
+	public static void main(String[] args)
+	{
+		System.out.println("Print no 1 to 10 : ");
+		int startLoop = 1;
+		int endLoop = 10;
+		
+		for (int i = startLoop; i <= endLoop; i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
