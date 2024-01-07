e27af3aeb59c3bdeff4135c58b1840cd0e0dc51e
sv-aloksingh <aloksingh8979@gmail.com>
2024-01-07 10:46:47 -0800

    Odd even number

--- src/DSALectureByFrazPulkit/LectureProgramsPractice/EvenOddNumber.java
+++ src/DSALectureByFrazPulkit/LectureProgramsPractice/EvenOddNumber.java
@@ -1,9 +1,26 @@
 package DSALectureByFrazPulkit.LectureProgramsPractice;
+import java.util.Scanner;
 
 public class EvenOddNumber {
 	public static void main(String[] args)
 	{
-		//Input a number, Check if its even or odd and print it. 
-		
+		//Input a number, Check if its even or odd and print it. (it can be any no positive or negative including zero)
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int input = sc.nextInt();
+		/*
+		if (input == 0) 
+		{
+			System.out.println("Even");
+		} 
+		*/
+		if (input % 2 == 0)
+		{
+			System.out.println("Even");
+		}
+		else
+		{
+			System.out.println("Odd");
+		}
 	}
 }
