6ad8814ee8a0e069e2c7c8adfac944e707d9bdbc
sv-aloksingh <aloksingh8979@gmail.com>
2024-01-07 10:17:17 -0800

    input a string by NextLine

--- src/SelfJavaLearning/inputAString.java
+++ src/SelfJavaLearning/inputAString.java
@@ -1,5 +1,12 @@
 package SelfJavaLearning;
+import java.util.Scanner;
 
 public class inputAString {
-
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string: ");
+		String str = sc.nextLine();
+		System.out.println("Entered string = " + str);
+	}
 }
