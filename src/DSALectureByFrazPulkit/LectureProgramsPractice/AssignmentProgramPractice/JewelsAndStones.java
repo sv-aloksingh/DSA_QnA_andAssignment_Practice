99dba0568fc8463335b4f12d4225e946dab093dc
sv-aloksingh <aloksingh8979@gmail.com>
2024-01-26 12:07:51 +0530

    jewels and stones

--- src/DSALectureByFrazPulkit/LectureProgramsPractice/AssignmentProgramPractice/JewelsAndStones.java
+++ src/DSALectureByFrazPulkit/LectureProgramsPractice/AssignmentProgramPractice/JewelsAndStones.java
@@ -5,16 +5,43 @@ public class JewelsAndStones {
 	public static void main(String[] arg)
 	{
 		String jewels = "aA", stones = "aAAbbbb"; //expected output 3 
-		
+		System.out.print("Bruteforce result = " + BruteforceApproach(jewels, stones));
+		System.out.print("\nOptimal Apprach = " + OptimalApproach(jewels, stones));
 	}
 	
 	public static int BruteforceApproach(String jewels, String stones)
 	{
-		return 1;
+		int count = 0;
+		for (int i =0;i< jewels.length();++i)
+		{
+			for (int j=0; j < stones.length(); ++j)
+			{
+				if (jewels.charAt(i) == stones.charAt(j))
+				{
+					count++;
+				}
+			}
+		}
+		return count;
 	}
-	
 	public static int OptimalApproach(String jewels, String stones)
 	{
-		return 1;
+		System.out.println();
+		char[] my256charCountArray = new char[256];
+		for (int i = 0; i< stones.length(); i++)
+		{
+			int myCharInts = stones.charAt(i);
+			my256charCountArray[myCharInts]++;
+		}
+		
+		int count = 0;
+		for (int j = 0; j< jewels.length();++j)
+		{
+			int jewelsCharIntToFindIn256CharArray = jewels.charAt(j);
+			count = count + my256charCountArray[jewelsCharIntToFindIn256CharArray];
+			System.out.println(jewels.charAt(j) +" = "+ (int)my256charCountArray[jewelsCharIntToFindIn256CharArray]);
+		}
+		
+		return count;
 	}
 }
