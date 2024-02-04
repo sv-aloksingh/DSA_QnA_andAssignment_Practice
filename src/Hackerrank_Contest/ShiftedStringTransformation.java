08d05b0aeb34df31609f6997b457fc7d2712827e
sv-aloksingh <aloksingh8979@gmail.com>
2024-02-05 00:05:27 +0530

    Shifted string contest problem

--- src/Hackerrank_Contest/ShiftedStringTransformation.java
+++ src/Hackerrank_Contest/ShiftedStringTransformation.java
@@ -1,5 +1,36 @@
 package Hackerrank_Contest;
+import java.util.Scanner;
 
 public class ShiftedStringTransformation {
-
+	public static void main(String[] arg)
+	{
+		/* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+        char[] charA = A.toCharArray();
+        char[] charB = B.toCharArray();
+        
+        
+        if (A.length() == B.length())
+        {
+        	int diff = 0;
+            int leftShift = -1;
+            String result = "Yes";
+        	
+        	for (int i = 0; i<A.length(); i++)
+        	{
+        		diff = charA[i] - charB[i];
+        		if (diff < 0)
+        			diff = charA[i] - (charB[i] - 26);
+        		if (leftShift != -1 && leftShift != diff)
+    			{
+    				result = "No";
+    				break;
+    			}
+    			leftShift = diff;
+        	}
+        	System.out.print(result);
+        }
+	}
 }
