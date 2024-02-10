457929a09fb671a68f3bfd9eb55d4bf501e4074c
sv-aloksingh <aloksingh8979@gmail.com>
2024-02-11 00:56:50 +0530

    arraylist in java

---
+++ src/DSALectureByFrazPulkit/LectureProgramsPractice/ArrayListInJava.java
@@ -0,0 +1,33 @@
+package DSALectureByFrazPulkit.LectureProgramsPractice;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import SelfJavaLearning.inputAString;
+
+public class ArrayListInJava {
+	public static void main(String[] arr)
+	{
+		ArrayList<Integer> list = new ArrayList();
+		AddMe(list, 3);
+		AddMe(list, 1);
+		AddMe(list, 19);
+		AddMe(list, 0);
+		AddMe(list, 99);
+		AddMe(list, 10);
+		
+		for(int i =0;i<list.size();i++)
+		{
+			System.out.print(list.get(i) + " ");
+		}
+	}
+	
+	public static void AddMe(ArrayList<Integer> list, int a)
+	{
+		list.add(a);
+	}
+	
+	public static void RemoveMe(ArrayList<Integer> list, int a)
+	{
+		list.remove(a);
+	}
+}
